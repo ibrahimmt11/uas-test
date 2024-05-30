b9d05889ffdede449bb7f26a2505eea3d14f54d6
ibrahimmt11 <abitmulyo@gmail.com>
2024-05-30 19:55:02 +0700

    add test file

---
+++ src/test/java/test.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import com.mycompany.tp_mod_02_1302210022_ibrahim_mulyo_tsabit.TP_MOD_02_1302210022_Ibrahim_Mulyo_Tsabit;
+import static org.junit.Assert.assertEquals;
+
+public class test {
+
+    @Test
+    public void testFuncfpb() {
+        // Test case 1: FPB dari 12 dan 18 adalah 6
+        int a = 12;
+        int b = 18;
+        int expectedFpb = 6;
+        int actualFpb = TP_MOD_02_1302210022_Ibrahim_Mulyo_Tsabit.funcfpb(a, b);
+        assertEquals(expectedFpb, actualFpb);
+
+        // Test case 2: FPB dari 24 dan 36 adalah 12
+        a = 24;
+        b = 36;
+        expectedFpb = 12;
+        actualFpb = TP_MOD_02_1302210022_Ibrahim_Mulyo_Tsabit.funcfpb(a, b);
+        assertEquals(expectedFpb, actualFpb);
+
+        // Test case 3: FPB dari 10 dan 15 adalah 5
+        a = 10;
+        b = 15;
+        expectedFpb = 5;
+        actualFpb = TP_MOD_02_1302210022_Ibrahim_Mulyo_Tsabit.funcfpb(a, b);
+        assertEquals(expectedFpb, actualFpb);
+    }
+}
